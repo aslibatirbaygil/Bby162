fbac3fb553ee223b96c861570ed31ca661c7527e
aslibatirbaygil <36676374+aslibatirbaygil@users.noreply.github.com>
2018-02-28 01:18:09 +0300

    Add files via upload

---
+++ uygulama01.py/uygulama01.py
@@ -0,0 +1,4 @@
+metin = "Büyük tufan kopmuş, yıkıntıların arasındayız şimdi, yeni yeni küçük yaşam alanları kurmaya, küçük küçük umutlar beslemeye başladık."
+metin = input("metni girin: ")
+metnin_first = metin[:int(len(metin)/2)]
+print(metnin_yarisi)
